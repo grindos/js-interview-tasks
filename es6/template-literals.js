140771d29ba7ba96e2e12824cbf960c440db0280
grindos <grisha_ya@inbox.ru>
2018-06-24 14:07:42 +0300

    Add 'template literal' case

---
+++ es6/template-literals.js
@@ -0,0 +1,15 @@
+// ES5
+
+var first = 'Gregory'
+var second = 'Yanushkovsky'
+
+console.log('My name is  ' + first + ' ' + second + '.')
+
+// ES6
+
+// In ECMAScript 6 there were added template literals. instead of ' or " you should use ` and ${} for the variable.
+
+const first = 'Gregory'
+const second = 'Yanushkovsky'
+
+console.log(`My name is ${first} ${second}.`)
