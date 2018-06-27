a831f610cd099fc2ae28c599f8081fa63712c71d
grindos <grisha_ya@inbox.ru>
2018-06-27 17:57:44 +0300

    Add 'difference-among-calls' task

---
+++ prototype-inheritance/difference-among-calls.js
@@ -0,0 +1,23 @@
+// We have the next code:
+
+function Rabbit (name) {
+  this.name = name
+}
+
+Rabbit.prototype.sayHi = function () {
+  console.log(this.name)
+}
+
+let rabbit = new Rabbit('Morty')
+
+// What next calls will print?
+
+rabbit.sayHi() // (1)
+Rabbit.prototype.sayHi() // (2)
+Object.getPrototypeOf(rabbit).sayHi() // (3)
+rabbit.__proto__.sayHi() // (4)
+
+/*
+(1) Morty. 'rabbit' doesn't have 'sayHi' method, but it will be found in prototype. 'this' will be a 'rabbit'.
+(2-4) undefined. 'this' will reference to Rabbit.prototype, which doesn't have a 'name' field, so it is undefined.
+ */
