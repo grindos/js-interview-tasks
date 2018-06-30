275f94f0bd541fa46cbb0b68777112716e9bd7ae
grindos <grisha_ya@inbox.ru>
2018-06-30 20:25:34 +0300

    Add ES6 part of 'classes' case

--- es6/classes.js
+++ es6/classes.js
@@ -16,3 +16,38 @@
   let animal = new Animal('Cat');
   animal.speak()
 }
+
+// Another implementation
+
+// let Animal = function(name) {
+//   this.name = name
+// }
+//
+// Animal.prototype.speak = function () {
+//   console.log(this.name + ' make a noise')
+// }
+//
+// let animal = new Animal('Cat')
+// animal.speak()
+
+// ---------- ES6 ----------
+
+{
+  class Animal {
+    constructor(name) {
+      this.name = name
+    }
+    speak() {
+      console.log(this.name + ' makes a noise.')
+    }
+  }
+
+  let animal = new Animal('Cat')
+  animal.speak()
+}
+
+// 'class' syntax allows you to avoid manipulating 'prototype' directly.
+// It makes the code more concise and easier to understand.
+
+// You better avoid having an empty constructor.
+// Classes have a default constructor if one is not specified.
