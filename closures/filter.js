10a07c8bf4b2e545d53025f5732a36879c65bcff
grindos <grisha_ya@inbox.ru>
2018-07-13 00:03:43 +0300

    Fix indentation

--- closures/filter.js
+++ closures/filter.js
@@ -8,24 +8,22 @@
 
 function filter(arr, func) {
   let res = []
-
   arr.forEach((element) => {
     if (func(element) === true) {
       res.push(element)
     }
   })
-
   return res
 }
 
 function inBetween (left, right) {
-  return function(a) {
+  return (a) => {
     return (a >= left && a <= right)
   }
 }
 
 function inArray (arr) {
-  return function(a) {
+  return (a) => {
     for(let i = 0; i < arr.length; i++) {
       if (arr[i] === a) {
         return true
@@ -35,12 +33,9 @@ function inArray (arr) {
   }
 }
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
-
-console.log(filter(arr, function(a) {
+let arr = [1, 2, 3, 4, 5, 6, 7]
+console.log(filter(arr, (a) => {
   return a % 2 === 0
-})); // 2,4,6
-
-console.log( filter(arr, inBetween(3, 6)) ); // 3,4,5,6
-
-console.log( filter(arr, inArray([1, 2, 10])) ); // 1,2
+})) // 2,4,6
+console.log( filter(arr, inBetween(3, 6)) ) // 3,4,5,6
+console.log( filter(arr, inArray([1, 2, 10])) ) // 1,2
