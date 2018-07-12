867c37d625bd74838c7c43e2d046da5dc3dd97ca
grindos <grisha_ya@inbox.ru>
2018-07-12 23:29:32 +0300

    Add 'sum' and 'string-buffer' tasks about closures

---
+++ closures/string-buffer.js
@@ -0,0 +1,33 @@
+/*
+*  2. Implement string buffer using functions, with the next syntax:
+*
+*  Create buffer: var buffer = makeBuffer()
+*  Add value to buffer: buffer(value)
+*  Return buffer: buffer()
+*/
+
+function makeBuffer() {
+  let buf = ''
+  return function(str) {
+    if (arguments.length === 0) {
+      return buf
+    }
+    buf += str
+  }
+}
+
+let buffer1 = makeBuffer();
+
+// Add values to buffer
+buffer1('abc');
+buffer1('def');
+buffer1('ghi');
+
+console.log( buffer1() ) // Should return 'abcdefghi'
+
+let buffer2 = makeBuffer();
+buffer2(0);
+buffer2(1);
+buffer2(0);
+
+console.log( buffer2() ); // Should return '010'
---
+++ closures/sum.js
@@ -0,0 +1,11 @@
+/*
+*  1. Write sum function, which will work next way: sum.js(a)(b) = a + b.
+*/
+
+function sum(leftArg) {
+  return function (rightArg) {
+    return leftArg + rightArg
+  }
+}
+
+console.log(sum(5)(6))
