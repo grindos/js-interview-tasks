0672622ac1f6e3b4cb266139d3edf18ba67adb66
grindos <grisha_ya@inbox.ru>
2018-07-23 18:42:01 +0300

    Update 'sum' task about closures with many parentheses case

--- closures/sum.js
+++ closures/sum.js
@@ -8,4 +8,31 @@ function sum(leftArg) {
   }
 }
 
-console.log(sum(5)(6))
+console.log("Sum: " + sum(5)(6))
+
+/*
+ * Now write sumAdvanced function which will work with any number of parentheses.
+ */
+
+function sumAdvanced(arg1) {
+
+  let currentSum = arg1
+
+  function f(arg2) {
+    currentSum += arg2
+    return f
+  }
+
+  f.toString = function() {
+    return currentSum
+  }
+
+  return f
+}
+
+
+// We can call .toString method of this function explicitly or we can just add string concat
+console.log("Sum: " + sumAdvanced(5))
+console.log("Sum: " + sumAdvanced(5)(6))
+console.log("Sum: " + sumAdvanced(5)(6)(7))
+console.log("Sum: " + sumAdvanced(5)(6)(7)(8))
