d023810e7c6ee80d322e69687d5a63a60a8bfe32
grindos <grisha_ya@inbox.ru>
2018-07-23 12:35:00 +0300

    Add 'what-will-be-printed' task about 'this'

---
+++ this/what-will-be-printed.js
@@ -0,0 +1,44 @@
+// What will be printed in these three cases?
+
+// 1.
+
+let firstUser = {
+  name: 'Vasya',
+  export: this
+}
+
+console.log(firstUser.export.name)
+
+// 2.
+
+let name = ''
+
+let secondUser = {
+  name: 'Vasya',
+  export: function() {
+    return this
+  }
+}
+
+console.log(secondUser.export().name)
+
+// 3.
+
+let thirdUser = {
+  name: 'Vasya',
+  export: function() {
+    return {
+      value: this
+    }
+  }
+}
+
+console.log(thirdUser.export().value.name)
+
+/*
+ * 1. undefined. 'this' is placed out of any function so it will reference to the global object.
+ * 2. Vasya. Since 'this' is placed in function it will reference to the object before dot.
+ *    In this case it is secondUser.
+ * 3. Vasys. 'this' is placed in object which is in function.
+ *    It means that it will reference to the object before function and it will ignore the closest object.
+ */
